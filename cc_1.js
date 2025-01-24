02eb5ad7d3e0b36a7edc22336370d4f929a353ea
ssj2-z <ssj2@usf.edu>
2025-01-23 20:05:42 -0500

    "Task 1 - Employee Information."

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+let employeeName = "joey";
+const employeeID = 33610;
+var isActive = true;
+console.log(employeeName);
+console.log(typeof employeeName);
+console.log(employeeID);
+console.log(typeof employeeID);
+console.log(isActive);
+console.log(typeof isActive);
